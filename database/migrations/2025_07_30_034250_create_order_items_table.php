5e4108866580531987786e8663b0655249d7d8c5
PakBass <andhibasuki@gmail.com>
2025-07-30 13:26:29 +0800

    first commit

---
+++ database/migrations/2025_07_30_034250_create_order_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('item_id');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->decimal('tax', 10, 2);
+            $table->decimal('total_price', 10, 2);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
